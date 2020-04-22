b4de82221bb9d08043b00788bc5bf04eeb240df2
AraqueGD <camiaraquecaro17@gmail.com>
2020-04-22 17:10:32 -0500

    [ADD]10. States and State

---
+++ web_flask/9-states.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+from models import storage
+from models.state import State
+from models.city import City
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_appcontext(error):
+    storage.close()
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    states = storage.all(State).values()
+    sort_states = sorted(list(states), key=lambda k: k.name)
+    cities = storage.all(City).values()
+    sort_cities = sorted(list(cities), key=lambda k: k.name)
+    return render_template('8-cities_by_states.html', states=sort_states,
+                           cities=sort_cities)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def states_id(id=""):
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda k: k.name)
+    flag_state = None
+    if (id is None):
+        states = None
+    else:
+        for idx_states in states:
+            if (idx_states == id):
+                flag_state = idx_states
+        cities = storage.all(City).values()
+        cities = sorted(list(cities), key=lambda k: k.name)
+        return render_template('9-states.html', states=states,
+                               cities=cities, flag_state=flag_state)
+
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
