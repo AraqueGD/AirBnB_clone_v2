57f60487700fb9fa7207dbfccb34c81ee7880aa9
AraqueGD <camiaraquecaro17@gmail.com>
2020-04-22 17:22:38 -0500

    Fix

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -12,16 +12,6 @@ def teardown_appcontext(error):
     storage.close()
 
 
-@app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
-    states = storage.all(State).values()
-    sort_states = sorted(list(states), key=lambda k: k.name)
-    cities = storage.all(City).values()
-    sort_cities = sorted(list(cities), key=lambda k: k.name)
-    return render_template('8-cities_by_states.html', states=sort_states,
-                           cities=sort_cities)
-
-
 @app.route('/states')
 @app.route('/states/<id>', strict_slashes=False)
 def states_id(id=None):
